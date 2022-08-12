ef49c315f7fcf5910068cc2704bdbb9fe63db178
yudeshui <599414006@qq.com>
2022-08-12 14:39:51 +0800

    feat: add page

--- src/components/graph/add/Add.vue
+++ src/components/graph/add/Add.vue
@@ -1,6 +1,6 @@
 <script lang='ts' setup>
 import { onBeforeMount, onMounted, reactive, ref, watch } from 'vue'
-import dayjs from 'dayjs'
+import { Plus } from '@element-plus/icons-vue'
 import * as echarts from 'echarts/core'
 import type { FormInstance, FormRules } from 'element-plus'
 import { ArrowLeft } from '@element-plus/icons-vue'
@@ -11,6 +11,15 @@ import { formatBarOptions, generateBarInstance } from '@/components/metrics/char
 import i18n from '@/i18n'
 
 type List = {name: string}[]
+type Link = {
+  id: string,
+  source_node: string,
+  source_link_field: string,
+  target_node: string,
+  target_link_field: string,
+  relationship: string
+
+}
 
 const emit = defineEmits(['toResult', 'toList'])
 
@@ -23,51 +32,57 @@ const checkedList = ref<string[]>([])
 const formLabelAlign = reactive<{
   database: string,
   jobName: string,
-  describe: string,
-  nodes: {filed: string, table: string, check: boolean, id: string}[]
-  links: any[]
+  desc: string,
+  nodes: {primary: string, table: string, check: boolean, id: string}[]
+  links: Link[]
 }>({
   database: '',
   jobName: '',
-  describe: '',
+  desc: '',
   nodes: [
     {
       id: '1',
       table: '1',
-      filed: '2',
+      primary: '2',
       check: false
     },
     {
       id: '1',
       table: '1',
-      filed: '2',
+      primary: '2',
       check: false
     },
     {
       id: '1',
       table: '1',
-      filed: '2',
+      primary: '2',
       check: false
     }
   ],
   links: [
     {
       id: '1',
-      table: '1',
-      filed: '2',
-      check: false
+      source_node: '1',
+      source_link_field: '2',
+      target_node: '1',
+      target_link_field: '1',
+      relationship: ''
     },
     {
-      id: '1',
-      table: '1',
-      filed: '2',
-      check: false
+      id: '2',
+      source_node: '1',
+      source_link_field: '2',
+      target_node: '1',
+      target_link_field: '1',
+      relationship: ''
     },
     {
-      id: '1',
-      table: '1',
-      filed: '2',
-      check: false
+      id: '3',
+      source_node: '1',
+      source_link_field: '2',
+      target_node: '1',
+      target_link_field: '1',
+      relationship: ''
     }
   ]
 })
@@ -133,6 +148,22 @@ const changeTable = () => {
 const queryField = () => {
   console.log('queryField')
 }
+
+const addLinks = () => {
+  formLabelAlign.links.push({
+    id: +new Date() + '',
+    source_node: '1',
+    source_link_field: '2',
+    target_node: '1',
+    target_link_field: '1',
+    relationship: ''
+  })
+}
+const deleteLink = (id: string) => {
+  const index = formLabelAlign.links.findIndex(item => item.id === id)
+  if (index < 0) return
+  formLabelAlign.links.splice(index, 1)
+}
 </script>
 <template>
   <section class="graph-add-container">
@@ -168,10 +199,10 @@ const queryField = () => {
 
       <el-form-item
         :label="$t('Describe')"
-        prop="describe"
+        prop="desc"
       >
         <el-input
-          v-model="formLabelAlign.describe"
+          v-model="formLabelAlign.desc"
           style="width: 600px"
           type="textarea"
           placeholder="Please input"
@@ -196,25 +227,26 @@ const queryField = () => {
           />
         </el-select>
       </el-form-item>
+
       <el-form-item
+        :label="$t('Nodes')"
         prop="nodes"
-        label-width="0"
       >
         <el-table
           :data="formLabelAlign.nodes"
           style="width: 100%"
         >
           <el-table-column
-            prop="date"
-            label="Date"
+            prop="check"
+            label="Check"
           >
             <template #default="scope">
               <el-checkbox v-model="scope.row.check" />
             </template>
           </el-table-column>
           <el-table-column
-            prop="name"
-            label="Name"
+            prop="table"
+            label="Table"
           >
             <template #default="scope">
               <el-select
@@ -234,12 +266,12 @@ const queryField = () => {
             </template>
           </el-table-column>
           <el-table-column
-            prop="address"
-            label="Address"
+            prop="primary"
+            label="Field"
           >
             <template #default="scope">
               <el-select
-                v-model="scope.row.table"
+                v-model="scope.row.primary"
                 popper-class="primary-select-dropdown" 
                 placeholder="Select Field"
                 filterable
@@ -255,33 +287,25 @@ const queryField = () => {
           </el-table-column>
         </el-table>
       </el-form-item>
+
       <el-form-item
-        prop="nodes"
-        label-width="0"
+        :label="$t('Links')"
+        prop="links"
       >
         <el-table
           :data="formLabelAlign.links"
           style="width: 100%;"
         >
           <el-table-column
-            prop="date"
-            label="Date"
-          >
-            <template #default="scope">
-              <el-checkbox v-model="scope.row.check" />
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="Name"
+            prop="source_node"
+            label="source_node"
           >
             <template #default="scope">
               <el-select
-                v-model="scope.row.table"
+                v-model="scope.row.source_node"
                 popper-class="primary-select-dropdown" 
-                placeholder="Select Table"
+                placeholder="Select source_node"
                 filterable
-                @change="changeTable"
               >
                 <el-option
                   v-for="item in tables"
@@ -293,12 +317,12 @@ const queryField = () => {
             </template>
           </el-table-column>
           <el-table-column
-            prop="address"
-            label="Address"
+            prop="source_link_field"
+            label="source_link_field"
           >
             <template #default="scope">
               <el-select
-                v-model="scope.row.table"
+                v-model="scope.row.source_link_field"
                 popper-class="primary-select-dropdown" 
                 placeholder="Select Field"
                 filterable
@@ -312,7 +336,85 @@ const queryField = () => {
               </el-select>
             </template>
           </el-table-column>
+          <el-table-column
+            prop="target_node"
+            label="target_node"
+          >
+            <template #default="scope">
+              <el-select
+                v-model="scope.row.target_node"
+                popper-class="primary-select-dropdown" 
+                placeholder="Select Field"
+                filterable
+              >
+                <el-option
+                  v-for="item in tables"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.name"
+                />
+              </el-select>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="target_link_field"
+            label="target_link_field"
+          >
+            <template #default="scope">
+              <el-select
+                v-model="scope.row.target_link_field"
+                popper-class="primary-select-dropdown" 
+                placeholder="Select Field"
+                filterable
+              >
+                <el-option
+                  v-for="item in tables"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.name"
+                />
+              </el-select>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="relationship"
+            label="relationship"
+          >
+            <template #default="scope">
+              <el-select
+                v-model="scope.row.relationship"
+                popper-class="primary-select-dropdown" 
+                placeholder="Select Field"
+                filterable
+              >
+                <el-option
+                  v-for="item in tables"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.name"
+                />
+              </el-select>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="operation"
+          >
+            <template #default="scope">
+              <span
+                class="link-del-btn"
+                @click="deleteLink(scope.row.id)"
+              >{{ $t('delete') }}</span>
+            </template>
+          </el-table-column>
         </el-table>
+        <span
+          class="add-link-btn"
+          @click="addLinks"
+        >
+          <el-icon :size="14">
+            <Plus />
+          </el-icon>
+        </span>
       </el-form-item>
     </el-form>
 
@@ -374,6 +476,25 @@ const queryField = () => {
 
   .width100>:deep(.el-input__wrapper) {
     max-width: 82px;
+  }
+
+  .add-link-btn {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    margin-top: 20px;
+    background-color: #FBFBFB;
+    border: 1px solid #F4F4F4;
+    cursor: pointer;
+  }
+
+  .link-del-btn {
+    display: inline-block;
+    text-align: center;
+    color: var(--el-color-primary);
+    cursor: pointer;
   }
 }
 </style>
