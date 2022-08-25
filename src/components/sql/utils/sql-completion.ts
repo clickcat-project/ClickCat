8e618b4de3ef576e196997f6473ba301e81a5183
yudeshui <599414006@qq.com>
2022-08-25 13:41:16 +0800

    feat: 代码提交

--- src/components/sql/utils/sql-completion.ts
+++ src/components/sql/utils/sql-completion.ts
@@ -174,110 +174,35 @@ function createCompleter(getExtraHints: any, tables: any[] = [], columns: any[])
       }
       return []
     }
-
-    // const selectLast = HasSpotTextArr[HasSpotTextArr.length - 1]
-    // const selectStr = HasSpotTextArr[HasSpotTextArr.length - 2]
-    // if (selectStr && columnsWantedKeyword.includes(selectStr.toLowerCase())) {
-    //   const noRepeatColumns = [...new Set(columns.map(item => {
-    //     return item.name
-    //   }))]
-    //   if (selectLast === '') {
-    //     return [
-    //       {
-    //         label: '*',
-    //         kind: monaco.languages.CompletionItemKind.Constant,
-    //         documentation: '*',
-    //         insertText: '*'
-    //       },
-    //       ...(noRepeatColumns.map(item => {
-    //         return {
-    //           label: item,
-    //           kind: monaco.languages.CompletionItemKind.Constant,
-    //           documentation: item,
-    //           insertText: item
-    //         }
-    //       }))
-    //     ]
-    //   } else {
-    //     return [
-    //       ...noRepeatColumns.filter(item => item.includes(selectLast)).map(item => {
-    //         return {
-    //           label: item,
-    //           kind: monaco.languages.CompletionItemKind.Constant,
-    //           documentation: item,
-    //           insertText: item
-    //         }
-    //       })
-    //     ]
-    //   }
-    // }
-
-    // if (hasSpotActiveStr.includes('.')) {
-    //   const activeStrArr = hasSpotActiveStr.split('.')
-    //   const last = activeStrArr[activeStrArr.length - 1]
-    //   if (!last) {
-    //     if (activeStrArr.length === 2) {
-    //       const database = activeStrArr[0]
-    //       const tableByDatabase = tables.filter(item => item.database === database)
-    //       return tableByDatabase.map(item => {
-    //         return getSuggestionItem(item)
-    //       })
-    //     } else if (activeStrArr.length === 3) {
-    //       // const tableName = activeStrArr[1]
-    //       // const columnsByTable = columns.filter(item => item.table === tableName)
-    //       // return columnsByTable.map(item => {
-    //       //   return getSuggestionItem(item)
-    //       // })
-    //       return []
-    //     }
-    //   } else if (last) {
-    //     if (activeStrArr.length === 2) {
-    //       const database = activeStrArr[0]
-    //       const tableByDatabase = tables.filter(item => item.database === database && item.name.includes(last))
-    //       return tableByDatabase.map(item => {
-    //         return getSuggestionItem(item)
-    //       })
-    //     } else if (activeStrArr.length === 3) {
-    //       // const tableName = activeStrArr[1]
-    //       // const columnsByTable = columns.filter(item => item.table === tableName && item.name.includes(last))
-    //       // return columnsByTable.map(item => {
-    //       //   return getSuggestionItem(item)
-    //       // })
-    //       return []
-    //     }
-    //   } else {
-    //     return []
-    //   }
-    // } else {
-      const testNoSpot = textNoSpecial.replace(/(\s+|\.)/g, ' ')
-      const noSpotTextArr = testNoSpot.split(/[\s;]/)
-      const noSpotActiveStr = noSpotTextArr[noSpotTextArr.length - 1]
-      const len = noSpotActiveStr.length
-      const rexp = new RegExp('([^\\w]|^)' + noSpotActiveStr + '\\w*', 'gim')
-      const match = textCurrent.match(rexp)
-      const textHints = !match ? [] :
-        match.map((ele: any) => {
-          const rexp = new RegExp(noSpotActiveStr, 'gim')
-          const search = ele.search(rexp)
-          return ele.substr(search)
-        })
-      const mergeHints = Array.from(new Set([...hasDatabaseHints, ...textHints, ...getExtraHints(model)]))
-        .sort()
-        .filter(ele => {
-          const rexp = new RegExp(ele.substr(0, len), 'gim')
-          return (match && match.length === 1 && ele === noSpotActiveStr) ||
-            ele.length === 1 ? false : noSpotActiveStr.match(rexp)
-        })
-      return mergeHints.map(ele => ({
-        label: ele,
-        kind: hasDatabaseHints.indexOf(ele) > -1 ?
-          monaco.languages.CompletionItemKind.Keyword :
-          monaco.languages.CompletionItemKind.Text,
-        documentation: ele,
-        insertText: ele
-      }))
-    // }
+    const testNoSpot = textNoSpecial.replace(/(\s+|\.)/g, ' ')
+    const noSpotTextArr = testNoSpot.split(/[\s;]/)
+    const noSpotActiveStr = noSpotTextArr[noSpotTextArr.length - 1]
+    const len = noSpotActiveStr.length
+    const rexp = new RegExp('([^\\w]|^)' + noSpotActiveStr + '\\w*', 'gim')
+    const match = textCurrent.match(rexp)
+    const textHints = !match ? [] :
+      match.map((ele: any) => {
+        const rexp = new RegExp(noSpotActiveStr, 'gim')
+        const search = ele.search(rexp)
+        return ele.substr(search)
+      })
+    const mergeHints = Array.from(new Set([...hasDatabaseHints, ...textHints, ...getExtraHints(model)]))
+      .sort()
+      .filter(ele => {
+        const rexp = new RegExp(ele.substr(0, len), 'gim')
+        return (match && match.length === 1 && ele === noSpotActiveStr) ||
+          ele.length === 1 ? false : noSpotActiveStr.match(rexp)
+      })
+    return mergeHints.map(ele => ({
+      label: ele,
+      kind: hasDatabaseHints.indexOf(ele) > -1 ?
+        monaco.languages.CompletionItemKind.Keyword :
+        monaco.languages.CompletionItemKind.Text,
+      documentation: ele,
+      insertText: ele
+    }))
   }
+  
   return {
     triggerCharacters: ['.'],
     provideCompletionItems(model: any, position: any) {
